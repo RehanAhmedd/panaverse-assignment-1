74243d68031f8ed7a4f40df8d85f8387d4a5496b
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 19:29:53 +0500

    Invitation

---
+++ Question 14/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let guestList = ['Muhammad', 'Usman', 'Umar'];
+const invitation = (guest) => {
+    console.log(`Dear ${guest},
+        I hope this message finds you well. I am writing to formally extend an invitation to a dinner I am hosting in the near future. I would be honored to have you as my guest, and I am sure that we could have an interesting and enlightening conversation.
+        Please let me know if you are available and if you would be interested in joining me for this occasion.
+        Best regards,
+        Rehan Ahmed`);
+};
+for (const guest of guestList) {
+    invitation(guest);
+}
