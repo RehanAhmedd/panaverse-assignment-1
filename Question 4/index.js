ec536cb168c49adb7429cbc119f136fbdd416cf6
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 14:23:55 +0500

    Print the quote and the name of its author.

---
+++ Question 4/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let famouseAuthor = "Bruce Lee";
+let quote = "Be Water, My Friend.\n" +
+    "Empty your mind.\n" +
+    "Be formless, shapeless, like water.\n" +
+    "You put water into a cup, it becomes the cup.\n" +
+    "You put water into a bottle, it becomes the bottle.\n" +
+    "You put it into a teapot, it becomes the teapot.\n" +
+    "Now water can flow or it can crash.\n" +
+    "Be water, my friend.";
+console.log(famouseAuthor + ' once said,"' + quote + '"');
---
+++ Question 4/index.ts
@@ -0,0 +1,11 @@
+let famouseAuthor: string = "Bruce Lee";
+let quote: string = "Be Water, My Friend.\n" +
+"Empty your mind.\n" +
+"Be formless, shapeless, like water.\n" +
+"You put water into a cup, it becomes the cup.\n" +
+"You put water into a bottle, it becomes the bottle.\n" +
+"You put it into a teapot, it becomes the teapot.\n" +
+"Now water can flow or it can crash.\n" +
+"Be water, my friend."
+
+console.log(famouseAuthor+ ' once said,"' + quote +'"'); 
