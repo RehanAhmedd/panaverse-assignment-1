91b6e60dc37185f797729027a50cce5a8a1051bf
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 14:58:18 +0500

    Adding Comments

---
+++ Question 10/index.js
@@ -0,0 +1,15 @@
+"use strict";
+/* This program store the famous quote of famous person in variable
+        and print it in console*/
+let quote = "Be Water, My Friend.\n" +
+    "Empty your mind.\n" +
+    "Be formless, shapeless, like water.\n" +
+    "You put water into a cup, it becomes the cup.\n" +
+    "You put water into a bottle, it becomes the bottle.\n" +
+    "You put it into a teapot, it becomes the teapot.\n" +
+    "Now water can flow or it can crash.\n" +
+    "Be water, my friend.";
+console.log('Bruce Lee once said,"' + quote + '"');
+// This program store my favorite number in variable and display it
+let myFavoriteNumber = 6;
+console.log("My favorite number is: " + myFavoriteNumber);
---
+++ Question 10/index.ts
@@ -0,0 +1,17 @@
+/* This program store the famous quote of famous person in variable 
+        and print it in console*/
+let quote: string = "Be Water, My Friend.\n" +
+"Empty your mind.\n" +
+"Be formless, shapeless, like water.\n" +
+"You put water into a cup, it becomes the cup.\n" +
+"You put water into a bottle, it becomes the bottle.\n" +
+"You put it into a teapot, it becomes the teapot.\n" +
+"Now water can flow or it can crash.\n" +
+"Be water, my friend."
+console.log('Bruce Lee once said,"' + quote +'"'); 
+
+// This program store my favorite number in variable and display it
+// Author: Rehan Ahmed
+// Date: 2023-02-11
+let myFavoriteNumber: number = 6;
+console.log("My favorite number is: "+ myFavoriteNumber);
