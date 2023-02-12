b6bf79d945d388e0fef7b1cfd0895c1c4de83a4f
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 14:35:59 +0500

    Stripping Names

---
+++ Question 6/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "\tRehan Ahmed\n";
+console.log("Before strinping the name: " + personName);
+personName = personName.trim();
+console.log("After strinping the name: " + personName);
---
+++ Question 6/index.ts
@@ -0,0 +1,4 @@
+let personName: string = "\tRehan Ahmed\n";
+console.log("Before strinping the name: "+ personName); 
+personName = personName.trim()
+console.log("After strinping the name: "+ personName); 
