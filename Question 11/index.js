42d0ee1281d852e1e0193485b8c9c4ca7a95d224
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 15:04:31 +0500

    friend names in array.

---
+++ Question 11/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Muhammad', 'Usman', 'Umair', 'Ateeq'];
+for (const name of names) {
+    console.log(name);
+}
---
+++ Question 11/index.ts
@@ -0,0 +1,4 @@
+let names: string[] = ['Muhammad', 'Usman', 'Umair', 'Ateeq']
+for (const name of names) {
+        console.log(name);
+}
