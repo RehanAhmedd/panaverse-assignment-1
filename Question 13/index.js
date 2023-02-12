01103a4dc35347b1e625a76a4d975c9223fea5c6
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-12 15:16:39 +0500

    favorite mode of transportation,

---
+++ Question 13/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['Crown', 'Suzuki Swift', 'Honda', 'Bmw m5'];
+for (const transport of transportation) {
+    console.log(`My favorite car is ${transport}`);
+}
---
+++ Question 13/index.ts
@@ -0,0 +1,4 @@
+let transportation: string[] = ['Crown', 'Suzuki Swift', 'Honda', 'Bmw m5']
+for (const transport of transportation) {
+        console.log(`My favorite car is ${transport}`);
+}
