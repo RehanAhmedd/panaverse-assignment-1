aa6ac49d7797eee67859446d8c99e5942cf4a921
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-18 01:04:27 +0500

    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
    • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    
    • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
    • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    
    • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!

---
+++ Question 34/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const favoritePizzas = ['Pepperoni', 'Margherita', 'Supreme'];
+// Printing pizza names
+for (const pizza of favoritePizzas) {
+    console.log(pizza);
+}
+// Printing a statement for each pizza
+for (const pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+// Printing an additional sentence
+console.log(`I really love pizza!. The pizza is delicious`);
