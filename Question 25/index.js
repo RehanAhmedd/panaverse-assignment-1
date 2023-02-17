dd773970371267052a35feff862461246a820b6d
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-18 00:14:43 +0500

    Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
    
    • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)

---
+++ Question 25/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let alien_color = ['green', 'red', 'yellow'];
+alien_color.forEach(color => {
+    if (color === 'green')
+        console.log('You just earned 5 points!');
+    else if (color === 'yellow')
+        console.log("");
+});
---
+++ Question 25/index.ts
@@ -0,0 +1,8 @@
+let alien_color: string[] = ['green','red','yellow'];
+
+
+alien_color.forEach(color=>{
+    if(color === 'green') console.log('You just earned 5 points!');
+    else if (color === 'yellow') console.log("");
+    
+})
