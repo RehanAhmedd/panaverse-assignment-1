37b7df47201cdaa1affea00c0aabf00cc5a445d1
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-17 23:44:12 +0500

    They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.

---
+++ Question 21/index.ts
@@ -0,0 +1,34 @@
+// create an object for each mobile
+const hawaii = {
+    make: "Hawaii",
+    model: "P20 Pro",
+    year: 2022,
+    color: "black"
+  };
+  
+  const oneplus = {
+    make: "Oneplus",
+    model: "Oneplus 8 pro",
+    year: 2021,
+    color: "black"
+  };
+
+  const oneplus8t = {
+    make: "Oneplus",
+    model: "Oneplus 8t",
+    year: 2020,
+    color: "black"
+  };
+
+  const apple = {
+    make: "Apple",
+    model: "Iphone 14 pro max",
+    year: 2022,
+    color: "black"
+  };
+  
+  // log the objects to the console
+  console.log(hawaii);
+  console.log(apple);
+  console.log(oneplus);
+  console.log(oneplus8t);
