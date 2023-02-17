a76912831ce4450b636d197efe0cc61a69eefa8f
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-17 23:38:00 +0500

    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ Question 20/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Random array
+let randomArray = [];
+randomArray.push('K2');
+randomArray.push('Sawat');
+randomArray.push('Fairy Meadows');
+randomArray.push('Prague');
+randomArray.push('Paris');
+randomArray.push('English');
+randomArray.push('Urdu');
+console.log("Some random mountains, rivers, countries, cities, languages names:");
+console.log(randomArray);
---
+++ Question 20/index.ts
@@ -0,0 +1,15 @@
+// Random array
+let randomArray : string[] = [];
+
+randomArray.push('K2');
+randomArray.push('Sawat');
+randomArray.push('Fairy Meadows');
+randomArray.push('Prague');
+randomArray.push('Paris');
+randomArray.push('English');
+randomArray.push('Urdu');
+
+console.log("Some random mountains, rivers, countries, cities, languages names:");
+console.log(randomArray);
+
+
