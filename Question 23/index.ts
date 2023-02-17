2847e608b07a9954c67d67f662165189434a8381
Rehan Ahmed <rehanahmed.iiui@gmail.com>
2023-02-18 00:02:13 +0500

    Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
    let car = 'subaru';
    
    console.log("Is car == 'subaru'? I predict True.")
    
    console.log(car == 'subaru')
    
    • Look closely at your results, and make sure you understand why each line evaluates to True or False.
    
    • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.

---
+++ Question 23/index.ts
@@ -0,0 +1,34 @@
+let number = 5;
+console.log(`Is ${number} equal to 5? I predict true.`);
+console.log(number == 5);
+
+console.log(`Is ${number} not equal to 5? I predict false.`);
+console.log(number != 5);
+
+console.log(`Is ${number} greater than 3? I predict true.`);
+console.log(number > 3);
+
+console.log(`Is ${number} less than 2? I predict false.`);
+console.log(number < 2);
+
+console.log(`Is ${number} greater than or equal to 5? I predict true.`);
+console.log(number >= 5);
+
+console.log(`Is ${number} less than or equal to 10? I predict true.`);
+console.log(number <= 10);
+
+let str1 = "hello";
+let str2 = "world";
+console.log(`Are ${str1} and ${str2} not equal? I predict true.`);
+console.log(str1 != str2);
+
+console.log(`Are ${str1} and ${str2} equal? I predict false.`);
+console.log(str1 == str2);
+
+let bool1 = true;
+let bool2 = false;
+console.log(`Is ${bool1} equal to true? I predict true.`);
+console.log(bool1 == true);
+
+console.log(`Is ${bool2} equal to true? I predict false.`);
+console.log(bool2 != false);
